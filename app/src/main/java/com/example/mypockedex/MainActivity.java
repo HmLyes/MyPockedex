b805bb846a0d648abb6665476299e42638c16047
HmLyes <hamiteche.lyes@gmail.com>
2019-03-25 16:45:42 +0100

    Animation intro

--- app/src/main/java/com/example/mypockedex/MainActivity.java
+++ app/src/main/java/com/example/mypockedex/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mypockedex;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int timer = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(MainActivity.this, ActivityMenu.class);
+                startActivity(welcome);
+                overridePendingTransition(R.anim.first, R.anim.end);
+                finish();
+            }
+        }, timer);
     }
+
 }
